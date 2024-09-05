4382b05a7dc2ab47589c7b1c10433540dcd4f506
DC-Chitoge <96510657+DC-Chitoge@users.noreply.github.com>
2024-09-05 14:59:00 +0700

    first commit

--- src/users/helper/admin.Permission.ts
+++ src/users/helper/admin.Permission.ts
@@ -3,7 +3,7 @@ import { User } from '../entities/user.entity';
 
 export class adminPermission {
   static check(currentUser: User) {
-    if (currentUser.role.toLowerCase() === 'rootadmin') return;
+    if (currentUser.role.toLowerCase() === 'admin') return;
     else {
       throw new BadRequestException(
         'User is not authorized to perform this action',
--- src/users/helper/update.permission.ts
+++ src/users/helper/update.permission.ts
@@ -2,14 +2,11 @@ import { BadRequestException } from '@nestjs/common';
 import { User } from '../entities/user.entity';
 
 export class updatePermission {
-  static check(id: number, currentUser: User) {
-    console.log(typeof currentUser.id, typeof id);
+  static async check(id: number, currentUser: User) {
     if (id === currentUser.id) return;
     if (currentUser.role.toLowerCase() === 'admin') return;
-    else {
-      throw new BadRequestException(
-        'User is not authorized to perform this action',
-      );
-    }
+    throw new BadRequestException(
+      'User is not authorized to perform this action',
+    );
   }
 }
